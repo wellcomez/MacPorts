wellcomez/MacPorts

commit 653091ceca0530026a859cad1f3c9c842834f875

port1.0/portlivecheck.tcl: Check for highest version in matching strings, in order to use file listings with multiple versions for livecheck
git-svn-id: http://svn.macports.org/repository/macports/trunk@34331 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portlivecheck.tcl
@@ -181,19 +181,22 @@ proc livecheck_main {args} {
                         ui_debug "The regex matched >$matched<"
                     }
                 } else {
+                    set updated_version 0
                     while {1} {
                         if {[gets $chan line] < 0} {
                             break
                         }
-                        if {[regexp $the_re $line matched updated_version]} {
-                            if {$updated_version != ${livecheck.version}} {
-                                set updated 1
-                            } else {
-                                set updated 0
+                        if {[regexp $the_re $line matched upver]} {
+                            if {[rpm-vercomp $upver $updated_version] > 0} {
+                                set updated_version $upver
                             }
                             ui_debug "The regex matched >$matched<"
-                            break
                         }
+                    }
+                    if {$updated_version != ${livecheck.version}} {
+                        set updated 1
+                    } else {
+                        set updated 0
                     }
                 }
                 close $chan

commit 085c89040fe43eea6b997d109a57573960fe351c

port1.0/portlivecheck.tcl: Fix a bug introduced in r34331, which printed a wrong error message. Closes #14532.
git-svn-id: http://svn.macports.org/repository/macports/trunk@34717 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portlivecheck.tcl
@@ -179,25 +179,28 @@ proc livecheck_main {args} {
                         } else {
                             set updated 0
                         }
-                        ui_debug "The regex matched \"$matched\""
+                        ui_debug "The regex matched \"$matched\", extracted \"$updated_version\""
                     }
                 } else {
                     set updated_version 0
-                    while {1} {
-                        if {[gets $chan line] < 0} {
-                            break
-                        }
+                    set foundmatch 0
+                    while {[gets $chan line] >= 0} {
                         if {[regexp $the_re $line matched upver]} {
+                            set foundmatch 1
                             if {[rpm-vercomp $upver $updated_version] > 0} {
                                 set updated_version $upver
                             }
-                            ui_debug "The regex matched \"$matched\""
+                            ui_debug "The regex matched \"$matched\", extracted \"$upver\""
                         }
                     }
-                    if {$updated_version != ${livecheck.version}} {
-                        set updated 1
-                    } else {
-                        set updated 0
+                    if {$foundmatch == 1} {
+                        if {$updated_version == 0} {
+                            set updated -1
+                        } elseif {$updated_version != ${livecheck.version}} {
+                            set updated 1
+                        } else {
+                            set updated 0
+                        }
                     }
                 }
                 close $chan