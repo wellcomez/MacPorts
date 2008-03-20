wellcomez/MacPorts

commit 1433e31c6fe5955d3b359c1e438e90fe199d9987

As written, every query built by the search menu selection has a "WHERE 1 AND <other criteria>" in it, which is absolutely unnecessary. Get rid of it and also rename $query variable to $criteria, to better reflect its intent as the query is being built.
git-svn-id: http://svn.macports.org/repository/macports/trunk@35151 d073be05-634f-4543-b044-5fe20cf6d1d6

www/ports.php
@@ -80,31 +80,30 @@
 
     if ($by && ($substr || $by == "all")) {
         $fields = "name, path, version, description";
-        $query = "1";
         $tables = "$portsdb_name.portfiles p";
         if ($by == "name") {
-            $query .= " AND p.name LIKE '%" . mysql_real_escape_string($substr) . "%'";
+            $criteria = "p.name LIKE '%" . mysql_real_escape_string($substr) . "%'";
         }
         if ($by == "category") {
             $tables .= ", $portsdb_name.categories c";
-            $query .= " AND c.portfile=p.name AND c.category='" . mysql_real_escape_string($substr) . "'";
+            $criteria = "c.portfile = p.name AND c.category = '" . mysql_real_escape_string($substr) . "'";
         }
         if ($by == "maintainer") {
             $tables .= ", $portsdb_name.maintainers m";
-            $query .= " AND m.portfile=p.name AND m.maintainer LIKE '%" . mysql_real_escape_string($substr) . "%'";
+            $criteria = "m.portfile = p.name AND m.maintainer LIKE '%" . mysql_real_escape_string($substr) . "%'";
         }
         if ($by == "library") {
-            $query .= " AND p.name='" . mysql_real_escape_string($substr) . "'";
+            $criteria = "p.name = '" . mysql_real_escape_string($substr) . "'";
         }
         if ($by == "variant") {
             $tables .= ", $portsdb_name.variants v";
-            $query .= " AND v.portfile=p.name AND v.variant='" . mysql_real_escape_string($substr) . "'";
+            $criteria = "v.portfile = p.name AND v.variant = '" . mysql_real_escape_string($substr) . "'";
         }
         if ($by == "platform") {
             $tables .= ", $portsdb_name.platforms pl";
-            $query .= " AND pl.portfile=p.name AND pl.platform ='" . mysql_real_escape_string($substr) . "'";
+            $criteria = "pl.portfile = p.name AND pl.platform = '" . mysql_real_escape_string($substr) . "'";
         }
-        $query = "SELECT DISTINCT $fields FROM $tables WHERE $query ORDER BY name";
+        $query = "SELECT DISTINCT $fields FROM $tables WHERE $criteria ORDER BY name";
         $result = mysql_query($query);
         if ($result) {
 ?>

commit 284da1b38b134d280a75d6b491e9adf55fb72277

Fix WHERE issue introduced in r35151 that broke the all ports listing
git-svn-id: http://svn.macports.org/repository/macports/trunk@35193 d073be05-634f-4543-b044-5fe20cf6d1d6

www/ports.php
@@ -103,7 +103,12 @@
             $tables .= ", $portsdb_name.platforms AS pl";
             $criteria = "pl.portfile = p.name AND pl.platform = '" . mysql_real_escape_string($substr) . "'";
         }
-        $query = "SELECT DISTINCT $fields FROM $tables WHERE $criteria ORDER BY name";
+        if ($criteria == "") {
+            $where = "";
+        } else {
+            $where = "WHERE $criteria";
+        }
+        $query = "SELECT DISTINCT $fields FROM $tables $where ORDER BY name";
         $result = mysql_query($query);
         if ($result) {
 ?>