wellcomez/MacPorts

commit dc887f9817e647ab99a00883fec2db35594bfae0

Move default frameworks_dir under prefix, and use it in portgroups
git-svn-id: http://svn.macports.org/repository/macports/trunk@40803 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/macports.tcl
@@ -1061,7 +1061,7 @@ proc _libtest {mport depspec} {
     if {[info exists env(DYLD_FRAMEWORK_PATH)]} {
         lappend search_path $env(DYLD_FRAMEWORK_PATH)
     } else {
-        lappend search_path /Library/Frameworks /Network/Library/Frameworks /System/Library/Frameworks
+        lappend search_path ${frameworks_dir} /Library/Frameworks /Network/Library/Frameworks /System/Library/Frameworks
     }
     if {[info exists env(DYLD_FALLBACK_FRAMEWORK_PATH)]} {
         lappend search_path $env(DYLD_FALLBACK_FRAMEWORK_PATH)
@@ -1069,7 +1069,7 @@ proc _libtest {mport depspec} {
     if {[info exists env(DYLD_LIBRARY_PATH)]} {
         lappend search_path $env(DYLD_LIBRARY_PATH)
     }
-    lappend search_path /lib /usr/lib /usr/X11R6/lib /usr/X11/lib ${prefix}/lib
+    lappend search_path /lib /usr/lib ${x11prefix}/lib ${prefix}/lib
     if {[info exists env(DYLD_FALLBACK_LIBRARY_PATH)]} {
         lappend search_path $env(DYLD_FALLBACK_LIBRARY_PATH)
     }

base/aclocal.m4
@@ -388,11 +388,7 @@ AC_DEFUN([MP_PATH_FRAMEWORKS],[
     AC_MSG_CHECKING([for Frameworks installation directory])
 
 	if test "x$MPFRAMEWORKSDIR" = "x" ; then
-	    if test "$DSTUSR" = "root" ; then
-		MPFRAMEWORKSDIR="/Library/Frameworks"
-	    else
-		MPFRAMEWORKSDIR="~$DSTUSR/Library/Frameworks"
-	    fi
+		MPFRAMEWORKSDIR="${prefix}/Library/Frameworks"
 	fi
 
 	AC_MSG_RESULT([$MPFRAMEWORKSDIR])

base/configure
@@ -6848,11 +6848,7 @@ fi
 echo $ECHO_N "checking for Frameworks installation directory... $ECHO_C" >&6; }
 
 	if test "x$MPFRAMEWORKSDIR" = "x" ; then
-	    if test "$DSTUSR" = "root" ; then
-		MPFRAMEWORKSDIR="/Library/Frameworks"
-	    else
-		MPFRAMEWORKSDIR="~$DSTUSR/Library/Frameworks"
-	    fi
+		MPFRAMEWORKSDIR="${prefix}/Library/Frameworks"
 	fi
 
 	{ echo "$as_me:$LINENO: result: $MPFRAMEWORKSDIR" >&5

base/doc/macports.conf.5
@@ -96,7 +96,7 @@ Directory containing Applications installed from ports.
 Directory containing Frameworks installed from ports.
 .br
 .Ic Default:
-/Library/Frameworks
+${prefix}/Library/Frameworks
 .It Va sources_conf
 Location of the sources file. This file enables rsync synchronization of the default ports tree with the
 MacPorts rsync server (through the "sync" target of the

base/doc/portgroup.7
@@ -142,9 +142,9 @@ Where to install the build product.
 .Em optional
 .br
 .Sy Default:
-.Em /Library/Frameworks
+.Em ${prefix}/Library/Frameworks
 or
-.Em /Application/MacPorts depending on xcode.destroot.type
+.Em /Applications/MacPorts depending on xcode.destroot.type
 .br
 .It Ic xcode.destroot.settings
 Additional settings passed to the xcode build tool during the destroot phase.

base/doc/porthier.7
@@ -53,6 +53,8 @@ standard C include files
 archive libraries
 .It Pa libexec/
 system daemons & system utilities (executed by other programs)
+.It Pa Library/Frameworks/
+Native Mac OS X Library Frameworks
 .It Pa sbin/
 system programs and administration utilities
 .It Pa share/
@@ -127,8 +129,6 @@ directory for cgi executables
 .El
 .It Pa /Applications/MacPorts/
 Native Mac OS X applications
-.It Pa /Library/Frameworks/
-Native Mac OS X Library Frameworks
 .Pp
 .El
 .El

base/src/port1.0/portmain.tcl
@@ -60,7 +60,7 @@ default worksymlink {[file join $portpath work]}
 default prefix /opt/local
 default x11prefix /usr/X11R6
 default applications_dir /Applications/MacPorts
-default frameworks_dir /Library/Frameworks
+default frameworks_dir {${prefix}/Library/Frameworks}
 default destdir destroot
 default destpath {${workpath}/${destdir}}
 # destroot is provided as a clearer name for the "destpath" variable

base/src/port1.0/resources/group/python24-1.0.tcl
@@ -34,9 +34,9 @@
 
 set python.bin	${prefix}/bin/python2.4
 set python.lib	${prefix}/lib/libpython2.4.dylib
-set python.libdir ${prefix}/Library/Frameworks/Python.framework/Versions/2.4/lib/python2.4
+set python.libdir ${frameworks_dir}/Python.framework/Versions/2.4/lib/python2.4
 set python.pkgd	${prefix}/lib/python2.4/site-packages
-set python.include	${prefix}/Library/Frameworks/Python.framework/Versions/2.4/include/python2.4
+set python.include	${frameworks_dir}/Python.framework/Versions/2.4/include/python2.4
 
 categories		python
 

commit 8b072b8c6835f10d46e33ad4e39db62770a33b34

Fix _libtest breakage introduced in r40803 (#16877)
git-svn-id: http://svn.macports.org/repository/macports/trunk@40834 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/macports1.0/macports.tcl
@@ -1054,9 +1054,11 @@ proc _mportsearchpath {depregex search_path {executable 0}} {
 # DYLD_FALLBACK_FRAMEWORK_PATH, and DYLD_FALLBACK_LIBRARY_PATH take precedence
 
 proc _libtest {mport depspec} {
-    global env tcl_platform macports::frameworks_dir macports::x11prefix
+    global env tcl_platform
     set depline [lindex [split $depspec :] 1]
     set prefix [_mportkey $mport prefix]
+    set frameworks_dir [_mportkey $mport frameworks_dir]
+    set x11prefix [_mportkey $mport x11prefix]
     
     if {[info exists env(DYLD_FRAMEWORK_PATH)]} {
         lappend search_path $env(DYLD_FRAMEWORK_PATH)