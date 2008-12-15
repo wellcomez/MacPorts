wellcomez/MacPorts

commit 0cc5c86cc5e37cc72bd75dd4b537b426deadf035

port/port.tcl - Addressed Ticket #13673. * `port -q info --index --maintainer all' now works. * Unfortunately it's still slow...
git-svn-id: http://svn.macports.org/repository/macports/trunk@43519 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port/port.tcl
@@ -1351,6 +1351,24 @@ proc action_info { action portlist opts } {
             array set portinfo [lindex $result 1]
             set porturl $portinfo(porturl)
             set portdir $portinfo(portdir)
+        } else {
+            # Extract the portdir from porturl and use it to search PortIndex.
+            # Only the last two elements of the path (porturl) make up the
+            # portdir.
+            set portdir [file split [macports::getportdir $porturl]]
+            set lsize [llength $portdir]
+            set portdir \
+                [file join [lindex $portdir [expr $lsize - 2]] \
+                           [lindex $portdir [expr $lsize - 1]]]
+            if {[catch {mportsearch $portdir no exact portdir} result]} {
+                ui_debug "$::errorInfo"
+                break_softcontinue "Portdir $portdir not found" 1 status
+            }
+            if {[llength $result] < 2} {
+                break_softcontinue "Portdir $portdir not found" 1 status
+            }
+            array unset portinfo
+            array set portinfo [lindex $result 1]
         }
 
         if {!([info exists options(ports_info_index)] && $options(ports_info_index) eq "yes")} {

commit a6acf992d1ec69ea8ab749eb6aa205267d0af9b0

src/port/port.tcl - special-case the current pseudo-port (url file://.) from being searched in PortIndex since it doesn't work (and will get a more meaningful message a few lines after the location of this change); otherwise, ports not yet added to PortIndex will fail 'port info'.  Also, 'port info --index' on a port that is in PortIndex will also fail without this instead of it hitting the "does not work with 'current' pseudo-port" Regression from r43519
git-svn-id: http://svn.macports.org/repository/macports/trunk@43811 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port/port.tcl
@@ -1351,7 +1351,7 @@ proc action_info { action portlist opts } {
             array set portinfo [lindex $result 1]
             set porturl $portinfo(porturl)
             set portdir $portinfo(portdir)
-        } else {
+        } elseif {$porturl ne "file://."} {
             # Extract the portdir from porturl and use it to search PortIndex.
             # Only the last two elements of the path (porturl) make up the
             # portdir.