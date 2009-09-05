wellcomez/MacPorts

commit c5a5bf080fb22fe140f83c0b0a58df193c3a8644

port1.0: Create namespaces for the packages in port1.0, stop polluting the global namespace with many local helper functions.
git-svn-id: http://svn.macports.org/repository/macports/trunk@49087 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portload.tcl
@@ -34,14 +34,17 @@
 package provide portload 1.0
 package require portutil 1.0
 
-set org.macports.load [target_new org.macports.load load_main]
+set org.macports.load [target_new org.macports.load portload::load_main]
 target_runtype ${org.macports.load} always
 target_provides ${org.macports.load} load 
-target_requires ${org.macports.load} main
+target_requires ${org.macports.load} portload::main
+
+namespace eval portload {
+}
 
 set_ui_prefix
 
-proc load_main {args} {
+proc portload::load_main {args} {
     global startupitem.type startupitem.name startupitem.location startupitem.plist
     set launchctl_path ${portutil::autoconf::launchctl_path}
 

base/src/port1.0/portactivate.tcl
@@ -36,7 +36,7 @@
 package provide portactivate 1.0
 package require portutil 1.0
 
-set org.macports.activate [target_new org.macports.activate activate_main]
+set org.macports.activate [target_new org.macports.activate portactivate::activate_main]
 target_runtype ${org.macports.activate} always
 target_state ${org.macports.activate} no
 target_provides ${org.macports.activate} activate
@@ -46,9 +46,12 @@ if {[option portarchivemode] == "yes"} {
 	target_requires ${org.macports.activate} main fetch extract checksum patch configure build destroot install
 }
 
+namespace eval portactivate {
+}
+
 set_ui_prefix
 
-proc activate_main {args} {
+proc portactivate::activate_main {args} {
 	global portname portversion portrevision portvariants user_options portnotes
 	registry_activate $portname ${portversion}_${portrevision}${portvariants} [array get user_options]
 

base/src/port1.0/portbuild.tcl
@@ -33,10 +33,13 @@
 package provide portbuild 1.0
 package require portutil 1.0
 
-set org.macports.build [target_new org.macports.build build_main]
+set org.macports.build [target_new org.macports.build portbuild::build_main]
 target_provides ${org.macports.build} build
 target_requires ${org.macports.build} main fetch extract checksum patch configure
-target_prerun ${org.macports.build} build_start
+target_prerun ${org.macports.build} portbuild::build_start
+
+namespace eval portbuild {
+}
 
 # define options
 options build.target
@@ -48,7 +51,7 @@ commands build
 # defaults
 default build.asroot no
 default build.dir {${workpath}/${worksrcdir}}
-default build.cmd {[build_getmaketype]}
+default build.cmd {[portbuild::build_getmaketype]}
 default build.nice {${buildnicevalue}}
 default build.jobs {${buildmakejobs}}
 default build.pre_args {${build.target}}
@@ -57,7 +60,7 @@ default use_parallel_build yes
 
 set_ui_prefix
 
-proc build_getmaketype {args} {
+proc portbuild::build_getmaketype {args} {
     if {![exists build.type]} {
         return make
     }
@@ -103,7 +106,7 @@ proc build_getmaketype {args} {
     }
 }
 
-proc build_getnicevalue {args} {
+proc portbuild::build_getnicevalue {args} {
     if {![exists build.nice] || [string match "* *" [option build.cmd]]} {
         return ""
     }
@@ -114,7 +117,7 @@ proc build_getnicevalue {args} {
     return "nice -n $nice "
 }
 
-proc build_getmakejobs {args} {
+proc portbuild::build_getmakejobs {args} {
     # check if port allows a parallel build
     global use_parallel_build
     if {![tbool use_parallel_build]} {
@@ -136,7 +139,7 @@ proc build_getmakejobs {args} {
     return " -j$jobs"
 }
 
-proc build_start {args} {
+proc portbuild::build_start {args} {
     global UI_PREFIX build.asroot
     
     ui_msg "$UI_PREFIX [format [msgcat::mc "Building %s"] [option portname]]"
@@ -150,7 +153,7 @@ proc build_start {args} {
     
 }
 
-proc build_main {args} {
+proc portbuild::build_main {args} {
     global build.cmd
 
     set nice_prefix [build_getnicevalue]

base/src/port1.0/portchecksum.tcl
@@ -34,10 +34,13 @@
 package provide portchecksum 1.0
 package require portutil 1.0
 
-set org.macports.checksum [target_new org.macports.checksum checksum_main]
+set org.macports.checksum [target_new org.macports.checksum portchecksum::checksum_main]
 target_provides ${org.macports.checksum} checksum
 target_requires ${org.macports.checksum} main fetch
-target_prerun ${org.macports.checksum} checksum_start
+target_prerun ${org.macports.checksum} portchecksum::checksum_start
+
+namespace eval portchecksum {
+}
 
 # Options
 options checksums checksum.skip
@@ -68,7 +71,7 @@ set checksum_types_count [llength $checksum_types]
 # (4) first word is one of the checksums types.
 #
 # return yes if the syntax was correct, no if there was a problem.
-proc parse_checksums {checksums_str} {
+proc portchecksum::parse_checksums {checksums_str} {
 	global checksums_array all_dist_files checksum_types checksum_types_count
 
 	# Parse the string of checksums.
@@ -147,7 +150,7 @@ proc parse_checksums {checksums_str} {
 # Calculate the md5 checksum for the given file.
 # Return the checksum.
 #
-proc calc_md5 {file} {
+proc portchecksum::calc_md5 {file} {
 	return [md5 file $file]
 }
 
@@ -156,7 +159,7 @@ proc calc_md5 {file} {
 # Calculate the sha1 checksum for the given file.
 # Return the checksum.
 #
-proc calc_sha1 {file} {
+proc portchecksum::calc_sha1 {file} {
 	return [sha1 file $file]
 }
 
@@ -165,7 +168,7 @@ proc calc_sha1 {file} {
 # Calculate the rmd160 checksum for the given file.
 # Return the checksum.
 #
-proc calc_rmd160 {file} {
+proc portchecksum::calc_rmd160 {file} {
 	return [rmd160 file $file]
 }
 
@@ -173,7 +176,7 @@ proc calc_rmd160 {file} {
 #
 # Target prerun procedure; simply prints a message about what we're doing.
 #
-proc checksum_start {args} {
+proc portchecksum::checksum_start {args} {
 	global UI_PREFIX
 
 	ui_msg "$UI_PREFIX [format [msgcat::mc "Verifying checksum(s) for %s"] [option portname]]"
@@ -183,7 +186,7 @@ proc checksum_start {args} {
 #
 # Target main procedure. Verifies the checksums of all distfiles.
 #
-proc checksum_main {args} {
+proc portchecksum::checksum_main {args} {
 	global UI_PREFIX all_dist_files checksum_types checksums_array portverbose checksum.skip
 
 	# If no files have been downloaded, there is nothing to checksum.

base/src/port1.0/portclean.tcl
@@ -37,22 +37,25 @@ package provide portclean 1.0
 package require portutil 1.0
 package require Pextlib 1.0
 
-set org.macports.clean [target_new org.macports.clean clean_main]
+set org.macports.clean [target_new org.macports.clean portclean::clean_main]
 target_runtype ${org.macports.clean} always
 target_state ${org.macports.clean} no
 target_provides ${org.macports.clean} clean
 target_requires ${org.macports.clean} main
-target_prerun ${org.macports.clean} clean_start
+target_prerun ${org.macports.clean} portclean::clean_start
+
+namespace eval portclean {
+}
 
 set_ui_prefix
 
-proc clean_start {args} {
+proc portclean::clean_start {args} {
     global UI_PREFIX
     
     ui_msg "$UI_PREFIX [format [msgcat::mc "Cleaning %s"] [option portname]]"
 }
 
-proc clean_main {args} {
+proc portclean::clean_main {args} {
     global UI_PREFIX
 	global ports_clean_dist ports_clean_work ports_clean_archive
 	global ports_clean_all usealtworkpath
@@ -85,7 +88,7 @@ proc clean_main {args} {
     return 0
 }
 
-proc clean_altsource {args} {
+proc portclean::clean_altsource {args} {
     global usealtworkpath worksymlink
     
     set sourcepath [string map {"work" ""} $worksymlink] 
@@ -107,7 +110,7 @@ proc clean_altsource {args} {
 # Remove the directory where the distfiles reside.
 # This is crude, but works.
 #
-proc clean_dist {args} {
+proc portclean::clean_dist {args} {
 	global ports_force portname distpath dist_subdir distfiles
 
 	# remove known distfiles for sure (if they exist)
@@ -174,7 +177,7 @@ proc clean_dist {args} {
 	return 0
 }
 
-proc clean_work {args} {
+proc portclean::clean_work {args} {
     global portbuildpath worksymlink
 
 	if {[file isdirectory $portbuildpath]} {
@@ -196,7 +199,7 @@ proc clean_work {args} {
 	return 0
 }
 
-proc clean_archive {args} {
+proc portclean::clean_archive {args} {
 	global workpath portarchivepath portname portversion ports_version_glob
 
 	# Define archive destination directory and target filename

base/src/port1.0/portconfigure.tcl
@@ -34,10 +34,13 @@
 package provide portconfigure 1.0
 package require portutil 1.0
 
-set org.macports.configure [target_new org.macports.configure configure_main]
+set org.macports.configure [target_new org.macports.configure portconfigure::configure_main]
 target_provides ${org.macports.configure} configure
 target_requires ${org.macports.configure} main fetch extract checksum patch
-target_prerun ${org.macports.configure} configure_start
+target_prerun ${org.macports.configure} portconfigure::configure_start
+
+namespace eval portconfigure {
+}
 
 # define options
 commands configure autoreconf automake autoconf xmkmf
@@ -54,12 +57,12 @@ default xmkmf.cmd           xmkmf
 default xmkmf.dir           {${worksrcpath}}
 default use_configure       yes
 
-option_proc use_autoreconf  set_configure_type
-option_proc use_automake    set_configure_type
-option_proc use_autoconf    set_configure_type
-option_proc use_xmkmf       set_configure_type
+option_proc use_autoreconf  portconfigure::set_configure_type
+option_proc use_automake    portconfigure::set_configure_type
+option_proc use_autoconf    portconfigure::set_configure_type
+option_proc use_xmkmf       portconfigure::set_configure_type
 
-proc set_configure_type {option action args} {
+proc portconfigure::set_configure_type {option action args} {
     if {[string equal ${action} "set"] && [tbool args]} {
         switch $option {
             use_autoreconf {
@@ -90,19 +93,19 @@ default configure.mtune     {}
 options configure.optflags configure.cflags configure.cppflags configure.cxxflags configure.objcflags configure.ldflags configure.libs configure.fflags configure.f90flags configure.fcflags configure.classpath
 default configure.optflags  {-O2}
 # compiler flags section
-default configure.cflags    {[configure_get_cflags]}
+default configure.cflags    {[portconfigure::configure_get_cflags]}
 default configure.cppflags  {"-I${prefix}/include"}
-default configure.cxxflags  {[configure_get_cflags]}
-default configure.objcflags {[configure_get_cflags]}
+default configure.cxxflags  {[portconfigure::configure_get_cflags]}
+default configure.objcflags {[portconfigure::configure_get_cflags]}
 default configure.ldflags   {"-L${prefix}/lib"}
 default configure.libs      {}
-default configure.fflags    {[configure_get_cflags]}
-default configure.f90flags  {[configure_get_cflags]}
-default configure.fcflags   {[configure_get_cflags]}
+default configure.fflags    {[portconfigure::configure_get_cflags]}
+default configure.f90flags  {[portconfigure::configure_get_cflags]}
+default configure.fcflags   {[portconfigure::configure_get_cflags]}
 default configure.classpath {}
 
 # internal function to return the system value for CFLAGS/CXXFLAGS/etc
-proc configure_get_cflags {args} {
+proc portconfigure::configure_get_cflags {args} {
     global configure.optflags
     global configure.m32 configure.m64 configure.march configure.mtune
     set flags "${configure.optflags}"
@@ -142,30 +145,30 @@ options configure.universal_target configure.universal_sysroot configure.univers
 default configure.universal_target      {${universal_target}}
 default configure.universal_sysroot     {${universal_sysroot}}
 default configure.universal_archs       {${universal_archs}}
-default configure.universal_args        {[configure_get_universal_args]}
-default configure.universal_cflags      {[configure_get_universal_cflags]}
-default configure.universal_cppflags    {[configure_get_universal_cppflags]}
-default configure.universal_cxxflags    {[configure_get_universal_cflags]}
-default configure.universal_ldflags     {[configure_get_universal_ldflags]}
+default configure.universal_args        {[portconfigure::configure_get_universal_args]}
+default configure.universal_cflags      {[portconfigure::configure_get_universal_cflags]}
+default configure.universal_cppflags    {[portconfigure::configure_get_universal_cppflags]}
+default configure.universal_cxxflags    {[portconfigure::configure_get_universal_cflags]}
+default configure.universal_ldflags     {[portconfigure::configure_get_universal_ldflags]}
 
 # Select a distinct compiler (C, C preprocessor, C++)
 options configure.ccache configure.distcc configure.pipe configure.cc configure.cxx configure.cpp configure.objc configure.f77 configure.f90 configure.fc configure.javac configure.compiler
 default configure.ccache        {${configureccache}}
 default configure.distcc        {${configuredistcc}}
 default configure.pipe          {${configurepipe}}
-default configure.cc            {[configure_get_compiler cc]}
-default configure.cxx           {[configure_get_compiler cxx]}
-default configure.cpp           {[configure_get_compiler cpp]}
-default configure.objc          {[configure_get_compiler objc]}
-default configure.f77           {[configure_get_compiler f77]}
-default configure.f90           {[configure_get_compiler f90]}
-default configure.fc            {[configure_get_compiler fc]}
-default configure.javac         {[configure_get_compiler javac]}
-default configure.compiler      {[configure_get_default_compiler]}
+default configure.cc            {[portconfigure::configure_get_compiler cc]}
+default configure.cxx           {[portconfigure::configure_get_compiler cxx]}
+default configure.cpp           {[portconfigure::configure_get_compiler cpp]}
+default configure.objc          {[portconfigure::configure_get_compiler objc]}
+default configure.f77           {[portconfigure::configure_get_compiler f77]}
+default configure.f90           {[portconfigure::configure_get_compiler f90]}
+default configure.fc            {[portconfigure::configure_get_compiler fc]}
+default configure.javac         {[portconfigure::configure_get_compiler javac]}
+default configure.compiler      {[portconfigure::configure_get_default_compiler]}
 
 set_ui_prefix
 
-proc configure_start {args} {
+proc portconfigure::configure_start {args} {
     global UI_PREFIX
     global configure.compiler
     
@@ -202,7 +205,7 @@ proc configure_start {args} {
 }
 
 # internal function to determine canonical system name for configure
-proc configure_get_universal_system_name {args} {
+proc portconfigure::configure_get_universal_system_name {args} {
     global configure.universal_target configure.universal_archs
     set arch "unknown"
     switch -- ${configure.universal_archs} {
@@ -225,7 +228,7 @@ proc configure_get_universal_system_name {args} {
 }
 
 # internal function to determine the universal args for configure.cmd
-proc configure_get_universal_args {args} {
+proc portconfigure::configure_get_universal_args {args} {
     global configure.universal_archs
     set system [configure_get_universal_system_name]
     set params "--disable-dependency-tracking"
@@ -237,7 +240,7 @@ proc configure_get_universal_args {args} {
 }
 
 # internal function to determine the "-arch xy" flags for the compiler
-proc configure_get_universal_archflags {args} {
+proc portconfigure::configure_get_universal_archflags {args} {
     global configure.universal_archs
     set flags ""
     foreach arch ${configure.universal_archs} {
@@ -247,7 +250,7 @@ proc configure_get_universal_archflags {args} {
 }
 
 # internal function to determine the CPPFLAGS for the compiler
-proc configure_get_universal_cppflags {args} {
+proc portconfigure::configure_get_universal_cppflags {args} {
     global configure.universal_sysroot
     set flags ""
     # include sysroot in CPPFLAGS too (twice), for the benefit of autoconf
@@ -258,7 +261,7 @@ proc configure_get_universal_cppflags {args} {
 }
 
 # internal function to determine the CFLAGS for the compiler
-proc configure_get_universal_cflags {args} {
+proc portconfigure::configure_get_universal_cflags {args} {
     global configure.universal_sysroot configure.universal_target
     set flags [configure_get_universal_archflags]
     # these flags should be valid for C/C++ and similar compiler frontends
@@ -269,7 +272,7 @@ proc configure_get_universal_cflags {args} {
 }
 
 # internal function to determine the LDFLAGS for the compiler
-proc configure_get_universal_ldflags {args} {
+proc portconfigure::configure_get_universal_ldflags {args} {
     global configure.universal_sysroot configure.universal_target
     global os.arch
     set flags [configure_get_universal_archflags]
@@ -281,7 +284,7 @@ proc configure_get_universal_ldflags {args} {
 }
 
 # internal function to determine the default compiler
-proc configure_get_default_compiler {args} {
+proc portconfigure::configure_get_default_compiler {args} {
     global os.platform os.major
     set compiler ""
     switch -exact "${os.platform} ${os.major}" {
@@ -295,7 +298,7 @@ proc configure_get_default_compiler {args} {
 }
 
 # internal function to find correct compilers
-proc configure_get_compiler {type} {
+proc portconfigure::configure_get_compiler {type} {
     global configure.compiler prefix
     set ret ""
     switch -exact ${configure.compiler} {
@@ -438,7 +441,7 @@ proc configure_get_compiler {type} {
     return $ret
 }
 
-proc configure_main {args} {
+proc portconfigure::configure_main {args} {
     global [info globals]
     global worksrcpath use_configure use_autoreconf use_autoconf use_automake use_xmkmf
     global configure.env configure.pipe configure.cflags configure.cppflags configure.cxxflags configure.objcflags configure.ldflags configure.libs configure.fflags configure.f90flags configure.fcflags configure.classpath

base/src/port1.0/portdepends.tcl
@@ -33,21 +33,24 @@
 package provide portdepends 1.0
 package require portutil 1.0
 
+namespace eval portdepends {
+}
+
 # define options
 options depends_build depends_run depends_lib depends
 # Export options via PortInfo
 options_export depends_build depends_lib depends_run
 
-option_proc depends_build validate_depends_options
-option_proc depends_run validate_depends_options
-option_proc depends_lib validate_depends_options
+option_proc depends_build portdepends::validate_depends_options
+option_proc depends_run portdepends::validate_depends_options
+option_proc depends_lib portdepends::validate_depends_options
 
 # New option for the new dependency. We generate a warning because we don't handle this yet.
-option_proc depends validate_depends_options_new
+option_proc depends portdepends::validate_depends_options_new
 
 set_ui_prefix
 
-proc validate_depends_options {option action {value ""}} {
+proc portdepends::validate_depends_options {option action {value ""}} {
     global targets
     switch $action {
 		set {
@@ -63,6 +66,6 @@ proc validate_depends_options {option action {value ""}} {
     }
 }
 
-proc validate_depends_options_new {option action {value ""}} {
+proc portdepends::validate_depends_options_new {option action {value ""}} {
     ui_warn [msgcat::mc "depends option is not handled yet"]
 }

base/src/port1.0/portdestroot.tcl
@@ -34,11 +34,16 @@
 package provide portdestroot 1.0
 package require portutil 1.0
 
-set org.macports.destroot [target_new org.macports.destroot destroot_main]
+set org.macports.destroot [target_new org.macports.destroot portdestroot::destroot_main]
 target_provides ${org.macports.destroot} destroot
 target_requires ${org.macports.destroot} main fetch extract checksum patch configure build
-target_prerun ${org.macports.destroot} destroot_start
-target_postrun ${org.macports.destroot} destroot_finish
+target_prerun ${org.macports.destroot} portdestroot::destroot_start
+target_postrun ${org.macports.destroot} portdestroot::destroot_finish
+
+namespace eval portdestroot {
+    # Save old umask
+    variable oldmask
+}
 
 # define options
 options destroot.target destroot.destdir destroot.clean destroot.keepdirs destroot.umask
@@ -81,15 +86,11 @@ default startupitem.netchange   no
 
 set_ui_prefix
 
-namespace eval destroot {
-    # Save old umask
-    variable oldmask
-}
-
-proc destroot_start {args} {
+proc portdestroot::destroot_start {args} {
     global UI_PREFIX prefix portname porturl destroot os.platform destroot.clean portsharepath
-    global destroot::oldmask destroot.umask destroot.asroot macportsuser euid egid usealtworkpath altprefix
+    global destroot.umask destroot.asroot macportsuser euid egid usealtworkpath altprefix
     global applications_dir frameworks_dir
+    variable oldmask
     
     ui_msg "$UI_PREFIX [format [msgcat::mc "Staging %s into destroot"] ${portname}]"
 
@@ -137,19 +138,20 @@ proc destroot_start {args} {
     system "cd \"${destroot}/${prefix}\" && ${mtree} -e -U -f [file join ${portsharepath} install prefix.mtree]"
 }
 
-proc destroot_main {args} {
+proc portdestroot::destroot_main {args} {
     command_exec destroot
     return 0
 }
 
-proc destroot_finish {args} {
-    global UI_PREFIX destroot prefix portname startupitem.create destroot::oldmask destroot.violate_mtree
+proc portdestroot::destroot_finish {args} {
+    global UI_PREFIX destroot prefix portname startupitem.create destroot.violate_mtree
     global os.platform os.version
+    variable oldmask
 
     # Create startup-scripts/items
     if {[tbool startupitem.create]} {
         package require portstartupitem 1.0
-        startupitem_create
+        portstartupitem::startupitem_create
     }
 
     # Prune empty directories in ${destroot}

commit 1209c9fb56d25fb2851a81b837d5ee6cd0dc1807

port1.0/portload.tcl: Fix a bug introduced in r49087 which prevented 'port load' from running, ticket #21128
git-svn-id: http://svn.macports.org/repository/macports/trunk@57055 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portload.tcl
@@ -37,7 +37,7 @@ package require portutil 1.0
 set org.macports.load [target_new org.macports.load portload::load_main]
 target_runtype ${org.macports.load} always
 target_provides ${org.macports.load} load 
-target_requires ${org.macports.load} portload::main
+target_requires ${org.macports.load} main
 
 namespace eval portload {
 }