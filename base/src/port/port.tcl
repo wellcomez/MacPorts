wellcomez/MacPorts

commit 810403fa337168af1a1fc1d97bc7387ad9181c97

port/port.tcl: Check for errors when opening log file
git-svn-id: http://svn.macports.org/repository/macports/trunk@63269 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port/port.tcl
@@ -1473,7 +1473,9 @@ proc action_log { action portlist opts } {
         set portpath [macports::getportdir $porturl]
         set logfile [file join [macports::getportlogpath $portpath] "main.log"]
         if {[file exists $logfile]} {
-            set fp [open $logfile r]
+            if {[catch {set fp [open $logfile r]} result} {
+                break_softcontinue "Could not open file $logfile: $result" 1 status
+            }
             set data [read $fp]
             set data [split $data "\n"]
 

commit 2f504b95e32696b0ae8a547878eb602a2d7ec58b

port/port.tcl: Add missing close-bracket, fixes breakage introduced in r63269
git-svn-id: http://svn.macports.org/repository/macports/trunk@63271 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port/port.tcl
@@ -1472,7 +1472,7 @@ proc action_log { action portlist opts } {
         set portpath [macports::getportdir $porturl]
         set logfile [file join [macports::getportlogpath $portpath] "main.log"]
         if {[file exists $logfile]} {
-            if {[catch {set fp [open $logfile r]} result} {
+            if {[catch {set fp [open $logfile r]} result]} {
                 break_softcontinue "Could not open file $logfile: $result" 1 status
             }
             set data [read $fp]