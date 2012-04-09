wellcomez/MacPorts

commit 71afe55404c7cd621a1e782fa262e58acc61c50d

Auto-add depends_build bin:bsdmake:bsdmake to port setting build.type bsd, closes #33355
git-svn-id: http://svn.macports.org/repository/macports/trunk@90650 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/portdestroot.tcl
@@ -90,8 +90,9 @@ default startupitem.install     {$system_options(startupitem_install)}
 set_ui_prefix
 
 proc portdestroot::destroot_getargs {args} {
-    if {((![exists build.type] && [option os.platform] != "freebsd") || ([exists build.type] && [option build.type] == "gnu")) \
-        && [regexp "^(/\\S+/|)(g|gnu|)make(\\s+.*|)$" [option destroot.cmd]]} {
+    if {(([option build.type] == "default" && [option os.platform] != "freebsd") || \
+         ([option build.type] == "gnu")) \
+        && [regexp "^(/\\S+/|)(g|gnu|)make(\\s+.*|)$" [option build.cmd]]} {
         # Print "Entering directory" lines for better log debugging
         return "-w [option destroot.target]"
     }

base/src/port1.0/portbuild.tcl
@@ -43,9 +43,10 @@ namespace eval portbuild {
 }
 
 # define options
-options build.target
-options build.jobs
 options build.asroot
+options build.jobs
+options build.target
+options build.type
 options use_parallel_build
 commands build
 # defaults
@@ -56,12 +57,38 @@ default build.nice {${buildnicevalue}}
 default build.jobs {[portbuild::build_getjobs]}
 default build.pre_args {[portbuild::build_getargs]}
 default build.target "all"
+default build.type "default"
 default use_parallel_build yes
+
+# proc to add dependency on bsdmake, if necessary
+option_proc build.type portbuild::set_build_type
 
 set_ui_prefix
 
+proc portbuild::set_build_type {option action args} {
+    array set build_type_map {
+        bsd     {bin:bsdmake:bsdmake}
+    }
+
+    if {$action == "set"} {
+        switch $option {
+            build.type {
+                # using [exists foo] doesn't work with arrays!
+                if {[info exists build_type_map([option build.type])]} {
+                    # remove dependencies added before when setting build.type more than once
+                    eval depends_build-delete $build_type_map([option build.type])
+                }
+                if {[info exists build_type_map($args)]} {
+                    # add dependency if needed
+                    eval depends_build-append $build_type_map($args)
+                }
+            }
+        }
+    }
+}
+
 proc portbuild::build_getmaketype {args} {
-    if {![exists build.type]} {
+    if {[option build.type] == "default"} {
         return [findBinary make $portutil::autoconf::make_path]
     }
     switch -exact -- [option build.type] {
@@ -124,7 +151,8 @@ proc portbuild::build_getjobs {args} {
 }
 
 proc portbuild::build_getargs {args} {
-    if {((![exists build.type] && [option os.platform] != "freebsd") || ([exists build.type] && [option build.type] == "gnu")) \
+    if {(([option build.type] == "default" && [option os.platform] != "freebsd") || \
+         ([option build.type] == "gnu")) \
         && [regexp "^(/\\S+/|)(g|gnu|)make(\\s+.*|)$" [option build.cmd]]} {
         # Print "Entering directory" lines for better log debugging
         return "-w [option build.target]"

commit ac00d5a4e9d007ac0dce8be11b2fb0cf1a9447a8

Fix regression introduced by r90650 adding "-w" to destroot.cmd if build.cmd is gmake
Also adds a test for this problem, closes #33943.


git-svn-id: http://svn.macports.org/repository/macports/trunk@91744 d073be05-634f-4543-b044-5fe20cf6d1d6

base/src/port1.0/Makefile
@@ -33,3 +33,4 @@ install:: all
 
 test::
 	${TCLSH} tests/portutil.tcl ${macports_tcl_dir}
+	${TCLSH} tests/portdestroot.tcl

base/src/port1.0/portdestroot.tcl
@@ -92,7 +92,7 @@ set_ui_prefix
 proc portdestroot::destroot_getargs {args} {
     if {(([option build.type] == "default" && [option os.platform] != "freebsd") || \
          ([option build.type] == "gnu")) \
-        && [regexp "^(/\\S+/|)(g|gnu|)make(\\s+.*|)$" [option build.cmd]]} {
+        && [regexp "^(/\\S+/|)(g|gnu|)make(\\s+.*|)$" [option destroot.cmd]]} {
         # Print "Entering directory" lines for better log debugging
         return "-w [option destroot.target]"
     }