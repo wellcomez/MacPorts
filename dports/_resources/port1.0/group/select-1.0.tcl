wellcomez/MacPorts

commit 2fc659ae8494e28fba3cb9e8cd8c553889c42c1b

select-1.0: Allow multiple select files
Ports that wish to install multiple select files -- possibly for
multiple groups -- can now use the `select.entries` option. Assuming
that the `group1_select` and `group2_select` ports configure the
`group1` and `group2` groups:

    PortGroup       select 1.0
    ...
    depends_run     port:group1_select \
                    port:group2_select
    select.entries  {group1 foo} \
                    {group1 bar} \
                    {group2 baz quux}

This uses `${filespath}/foo` and `${filespath}/bar` to create "foo" and
"bar" entries in the `group1` group. An entry can have an optional third
argument if its name should differ from the name of the select file.
Above, `${filespath}/baz` creates a "quux" entry in the `group2` group.

Ports that only install one select file may continue using
`select.group` and `select.file`, if desired. If a portfile contains all
three of `select.group`, `select.file`, and `select.entries`, then
`select.group` and `select.file` are combined and prepended to
`select.entries`. (There's no reason to write a portfile this way,
though. Just use `select.entries`.)


git-svn-id: https://svn.macports.org/repository/macports/trunk@136723 d073be05-634f-4543-b044-5fe20cf6d1d6

dports/_resources/port1.0/group/select-1.0.tcl
@@ -1,10 +1,8 @@
 # et:ts=4
-# select-1.0.tcl
-#
 # $Id$
 #
-# Copyright (c) 2009 The MacPorts Project
 # Copyright (c) 2009 Rainer Mueller <raimue@macports.org>
+# Copyright (c) 2009-2015 The MacPorts Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -32,11 +30,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+#
+#
+# select-1.0.tcl
+#
+# This portgroup provides access to the port selection mechanism exposed
+# by the `port select` command. (Refer to the port(1) and port-select(1)
+# man pages for end-user information).
 
-options select.group select.file
+options select.group select.file select.entries
 
 default select.group {}
 default select.file {}
+default select.entries {}
 
 namespace eval select {}
 
@@ -65,9 +71,27 @@ proc select::install {group file {name ""}} {
 }
 
 post-destroot {
-    if {${select.file} ne "" && ${select.group} ne ""} {
-        select::install ${select.group} ${select.file}
-    } else {
-        ui_debug "PortGroup select: select.group or select.file not set"
+    if {${select.file} ne "" || ${select.group} ne ""} {
+        select.entries-prepend [list ${select.group} ${select.file}]
+    }
+    ui_debug {PortGroup select: Installing select files to destroot}
+    foreach entry ${select.entries} {
+        set extras [lassign $entry group file name]
+        if {[llength $extras] > 0} {
+            ui_debug "PortGroup select:\
+                    Ignoring entry with too many elements: '$entry'"
+            continue
+        }
+        if {$group eq ""} {
+            ui_debug "PortGroup select:\
+                    Ignoring entry with missing group name: '$entry'"
+            continue
+        }
+        if {$file eq ""} {
+            ui_debug "PortGroup select:\
+                    Ignoring entry with missing file name: '$entry'"
+            continue
+        }
+        select::install $group $file $name
     }
 }

commit 11b3058a257131ca9f3e1774bc3ca40772865f88

select-1.0: Don't overwrite `name` (#47849)
Also rename some other common-word variables.

Bug introduced in r136723. I forgot that portfile options are visible as
global variables in pre/post scripts.


git-svn-id: https://svn.macports.org/repository/macports/trunk@136794 d073be05-634f-4543-b044-5fe20cf6d1d6

dports/_resources/port1.0/group/select-1.0.tcl
@@ -75,23 +75,23 @@ post-destroot {
         select.entries-prepend [list ${select.group} ${select.file}]
     }
     ui_debug {PortGroup select: Installing select files to destroot}
-    foreach entry ${select.entries} {
-        set extras [lassign $entry group file name]
+    foreach selectEntry ${select.entries} {
+        set extras [lassign $selectEntry selectGroup selectFile selectName]
         if {[llength $extras] > 0} {
             ui_debug "PortGroup select:\
-                    Ignoring entry with too many elements: '$entry'"
+                    Ignoring entry with too many elements: '$selectEntry'"
             continue
         }
-        if {$group eq ""} {
+        if {$selectGroup eq ""} {
             ui_debug "PortGroup select:\
-                    Ignoring entry with missing group name: '$entry'"
+                    Ignoring entry with missing group name: '$selectEntry'"
             continue
         }
-        if {$file eq ""} {
+        if {$selectFile eq ""} {
             ui_debug "PortGroup select:\
-                    Ignoring entry with missing file name: '$entry'"
+                    Ignoring entry with missing file name: '$selectEntry'"
             continue
         }
-        select::install $group $file $name
+        select::install $selectGroup $selectFile $selectName
     }
 }